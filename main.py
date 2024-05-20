a543597b5bb1daef78b60a6664ed860aa8aadba5
Molly White <molly.white5@gmail.com>
2024-05-20 19:13:39 -0400

    Fetch race details

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Database import Database
 
 from committee_contributions import update_committee_contributions
 from committee_expenditures import update_committee_expenditures
+from races import update_race_details
 
 
 def main():
@@ -12,8 +13,8 @@ def main():
     db = Database()
     db.get_constants()
     # update_committee_contributions(db)
-    update_committee_expenditures(db)
-
+    # update_committee_expenditures(db)
+    update_race_details(db)
 
 if __name__ == "__main__":
     main()
